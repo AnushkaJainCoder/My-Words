6f4aa232e149e520d753d96dc0399d08f208fedf
Jain <anushka.jain@innovasolutions.com>
2024-11-20 12:56:10 +0530

    add form with cancel

--- src/Components/ShayriList.js
+++ src/Components/ShayriList.js
@@ -5,11 +5,13 @@ import AddShayri from "./AddShayri";
 
 export default function ShayriList(){
 
+    const [showForm, setShowForm] = useState(false)
     const  [shayris, setShayris] = useState([
         "Muskan Ban Jata Hai Koi,Dil Ki Dhadkan Ban Jata Hai Koi,Kaise Jiye Ek Pal Bhi Un Ke Bin,Jab Zindgi Jeene Ki Wajah Ban Jata Hai Koi,",
-        "eartbeats, the hum of your name,The sweetest song of my life.",
+        "Heartbeats, the hum of your name,The sweetest song of my life.",
         "love the way you are!"
     ]);
+
 
     const addShayri = (newShayri) => {
         setShayris([...shayris,newShayri])
@@ -21,7 +23,19 @@ export default function ShayriList(){
                  <ShayriItem key={index} shayri={shayri} />
             
             )    })}
-            <AddShayri addShayri = {addShayri} />
+            {!showForm &&(
+                <button onClick={() => setShowForm(true)}>
+                    Add
+                </button>
+            )}
+           
+            {showForm &&  <AddShayri addShayri = {addShayri} />}
+            {showForm && (
+                <button onClick={() => setShowForm(false)}>
+                    Cancel
+                </button>)
+            }
+           
             {/* <div> hello</div> */}
         </div>
     );
